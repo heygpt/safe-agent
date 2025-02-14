8de0d46bf2bfc669df0878eff406637d9c7e2dc5
Yohan <heyuni.gpt@gmail.com>
2025-02-14 09:44:43 +0800

    Add safe utilities.

---
+++ packages/plugins/safe/helpers/constants.ts
@@ -0,0 +1,4 @@
+export const ENSO_API_KEY =
+  process.env.ENSO_API_KEY ||
+  ('1e02632d-6feb-4a75-a157-documentation' as const);
+export const ENSO_ETH = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' as const;
---
+++ packages/plugins/safe/helpers/helper.ts
@@ -0,0 +1,70 @@
+import { Chain, createPublicClient, Hex, http } from 'viem';
+import {
+  arbitrum,
+  arbitrumSepolia,
+  base,
+  baseSepolia,
+  mainnet,
+  sepolia,
+} from 'viem/chains';
+import { getTokenInfo, maliciousCheck } from '../services/security-check';
+import { Check } from '../services/security-check/types';
+
+export const networkMappings: Record<number, Chain> = {
+  [mainnet.id]: mainnet,
+  [base.id]: base,
+  [arbitrum.id]: arbitrum,
+  [sepolia.id]: sepolia,
+  [baseSepolia.id]: baseSepolia,
+  [arbitrumSepolia.id]: arbitrumSepolia,
+};
+
+export async function isContractAddress(address: Hex, chain: Chain) {
+  try {
+    const client = createPublicClient({
+      chain,
+      transport: http(),
+    });
+
+    const bytecode = await client.getCode({ address });
+    return bytecode !== undefined && bytecode !== '0x';
+  } catch (error) {
+    console.error('Error checking if address is a contract:', error);
+    return false;
+  }
+}
+
+export function isAddressMalicious(checkResult: Check) {
+  return (
+    +checkResult.result.sanctioned === 1 ||
+    +checkResult.result.cybercrime === 1 ||
+    +checkResult.result.fake_token === 1
+  );
+}
+
+export async function addressValidation(
+  recipient: Hex,
+  tokenAddress: Hex,
+  chain: Chain
+) {
+  const checkResult = await maliciousCheck(recipient as Hex);
+  if (
+    +checkResult.result.sanctioned === 1 ||
+    +checkResult.result.cybercrime === 1 ||
+    +checkResult.result.fake_token === 1 ||
+    +checkResult.result.honeypot_related_address === 1
+  ) {
+    throw new Error(
+      `Heads up! This recipient address has been marked as malicious. Avoid interactions to protect your assets. ${JSON.stringify(checkResult.result)}`
+    );
+  }
+
+  if (!!tokenAddress && (await isContractAddress(tokenAddress as Hex, chain))) {
+    const tokenInfo = await getTokenInfo(tokenAddress, chain.id);
+    if (+tokenInfo.malicious_address === 1) {
+      throw new Error(
+        'Heads up! This token has been marked as malicious. Avoid interactions to protect your assets.'
+      );
+    }
+  }
+}
